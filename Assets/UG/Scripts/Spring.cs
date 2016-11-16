6aba432cbc2bf97dac80a707a01b0e251dc5988a
Robert Wells <robert.wells@gandhigames.co.uk>
2016-11-16 11:59:45 +0000

    initial commit

---
+++ Assets/UG/Scripts/Spring.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Spring
+{
+    public PointMass End1;
+    public PointMass End2;
+    public float TargetLength;
+    public float Stiffness;
+    public float Damping;
+
+    public Spring(PointMass end1, PointMass end2, 
+        float stiffness, float damping)
+    {
+        End1 = end1;
+        End2 = end2;
+        Stiffness = stiffness;
+        Damping = damping;
+        TargetLength = Vector3.Distance(end1.Position, end2.Position) * 0.95f;
+    }
+
+    public void Update()
+    {
+        var x = End1.Position - End2.Position;
+
+        float length = x.magnitude;
+        // these springs can only pull, not push
+        if (length <= TargetLength)
+            return;
+
+        x = (x / length) * (length - TargetLength);
+        var dv = End2.Velocity - End1.Velocity;
+        var force = Stiffness * x - dv * Damping;
+
+        End1.ApplyForce(-force);
+        End2.ApplyForce(force);
+    }
+
+    
+}
